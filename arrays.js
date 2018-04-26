72ce746fe12764cfa095d74ae4cb58df090ca215
Murat Celik <cashkl@yandex.com>
2018-04-26 17:11:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,19 +1,19 @@
 var chocolateBars=['snickers', 'hundred grand','kitkat','skittle']
-function addElementToBeginningOfArray(array,element) {  
-  arr.unshift(n)
-  return arr
+function addElementToBeginningOfArray(array,element) {
+  array.unshift(n)
+  return array
 }
 function destructivelyAddElementToBeginningOfArray(array,element) {
-  var arr2=arr;
+  var arr2=array;
   arr2[0]=n;
   return arr2
 }
 function addElementToEndOfArray(array,element) {
-  var arr2=arr
+  var arr2=array
   arr2.push(n)
   return arr2
 }
 function destructivelyAddElementToEndOfArray(array,element) {
   arr.last=n
-  return arr
+  return array
 }
